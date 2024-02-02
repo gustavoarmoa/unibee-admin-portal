6d19d9dd82fc79784a15f2b2e85ac9def58683bd
Edward <edward.wo@wowow.io>
2024-02-02 21:06:51 +0800

    styling

--- src/components/subscriptionDetail/userAccountTab.tsx
+++ src/components/subscriptionDetail/userAccountTab.tsx
@@ -1,4 +1,14 @@
-import { Button, Form, Input, Radio, Select, message } from "antd";
+import {
+  Button,
+  Col,
+  Divider,
+  Form,
+  Input,
+  Radio,
+  Row,
+  Select,
+  message,
+} from "antd";
 import { useNavigate } from "react-router-dom";
 import { Country, IProfile } from "../../shared.types";
 import { useEffect, useState } from "react";
@@ -119,12 +129,12 @@ const UserAccountTab = ({
     user != null && (
       <Form
         form={form}
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 24 }}
-        layout="horizontal"
+        labelCol={{ span: 7 }}
+        // wrapperCol={{ span: 24 }}
+        // layout="horizontal"
         onFinish={onSave}
         // disabled={componentDisabled}
-        style={{ maxWidth: 600 }}
+        // style={{ maxWidth: 600 }}
         initialValues={user}
       >
         <Form.Item label="id" name="id" hidden>
@@ -133,127 +143,181 @@ const UserAccountTab = ({
         <Form.Item label="countryName" name="countryName" hidden>
           <Input disabled />
         </Form.Item>
-
-        <Form.Item
-          label="First name"
-          name="firstName"
-          rules={[
-            {
-              required: true,
-              message: "Please input your first name!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Last name"
-          name="lastName"
-          rules={[
-            {
-              required: true,
-              message: "Please input your last name!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item label="Email" name="email">
-          <Input disabled />
-        </Form.Item>
-
-        <Form.Item
-          label="Billing address"
-          name="address"
-          rules={[
-            {
-              required: true,
-              message: "Please input your billing address!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Country"
-          name="countryCode"
-          rules={[
-            {
-              required: true,
-              message: "Please select your first country!",
-            },
-          ]}
-        >
-          <Select
-            showSearch
-            placeholder="Type to search"
-            optionFilterProp="children"
-            // value={country}
-            // onChange={onCountryChange}
-            // onSearch={onSearch}
-            filterOption={filterOption}
-            options={countryList.map((c) => ({
-              label: c.name,
-              value: c.code,
-            }))}
-          />
-        </Form.Item>
-
         <Form.Item label="Country Name" name="countryName" hidden>
           <Input />
         </Form.Item>
 
-        <Form.Item label="Company name" name="companyName">
-          <Input />
-        </Form.Item>
+        <Divider orientation="left" style={{ margin: "16px 0" }}>
+          General
+        </Divider>
+        <Row>
+          <Col span={12}>
+            <Form.Item
+              label="First name"
+              name="firstName"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your first name!",
+                },
+              ]}
+            >
+              <Input style={{ width: "240px" }} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Last name"
+              name="lastName"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your last name!",
+                },
+              ]}
+            >
+              <Input style={{ width: "240px" }} />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="VAT number" name="vATNumber">
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="Email" name="email">
+              <Input disabled style={{ width: "240px" }} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Company name" name="companyName">
+              <Input style={{ width: "240px" }} />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="Phone number" name="mobile">
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="VAT number" name="vATNumber">
+              <Input style={{ width: "240px" }} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Payment" name="paymentMethod">
+              <Radio.Group>
+                <Radio value="CreditCard">Credit Card</Radio>
+                <Radio value="Crypto">Crypto</Radio>
+                <Radio value="PayPal">PayPal</Radio>
+                <Radio value="WireTransfer">Wire Transfer</Radio>
+              </Radio.Group>
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="Telegram" name="telegram">
-          <Input />
-        </Form.Item>
+        <Divider orientation="left" style={{ margin: "16px 0" }}>
+          Contact Info
+        </Divider>
 
-        <Form.Item label="WhatsApp" name="whatsAPP">
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item
+              label="Country"
+              name="countryCode"
+              rules={[
+                {
+                  required: true,
+                  message: "Please select your first country!",
+                },
+              ]}
+            >
+              <Select
+                showSearch
+                placeholder="Type to search"
+                optionFilterProp="children"
+                // value={country}
+                // onChange={onCountryChange}
+                // onSearch={onSearch}
+                filterOption={filterOption}
+                options={countryList.map((c) => ({
+                  label: c.name,
+                  value: c.code,
+                }))}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Billing address"
+              name="address"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your billing address!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="WeChat" name="weChat">
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="Phone number" name="mobile">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="LinkedIn" name="linkedIn">
-          <Input />
-        </Form.Item>
+        <Divider orientation="left" style={{ margin: "16px 0" }}>
+          Social Info
+        </Divider>
 
-        <Form.Item label="Facebook" name="facebook">
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="Telegram" name="telegram">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="WhatsApp" name="whatsAPP">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="TikTok" name="tikTok">
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="WeChat" name="weChat">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="LinkedIn" name="linkedIn">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="Other social info" name="otherSocialInfo">
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item label="Facebook" name="facebook">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="TikTok" name="tikTok">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item label="Payment methods" name="paymentMethod">
-          <Radio.Group>
-            <Radio value="CreditCard">Credit Card</Radio>
-            <Radio value="Crypto">Crypto</Radio>
-            <Radio value="PayPal">PayPal</Radio>
-            <Radio value="WireTransfer">Wire Transfer</Radio>
-          </Radio.Group>
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            {" "}
+            <Form.Item label="Other social info" name="otherSocialInfo">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <div
           style={{
