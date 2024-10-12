2e712796b973d9002bb02bc78468ba0d6649a13e
The Mist <mist.zzh@gmail.com>
2024-10-12 16:22:43 +0800

    feat: add BASE_URL env varibale (#26)

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
+const DEFAULT_BASE_URL = '/'
+
+const BASE_URL_MAP = {
+  development: DEFAULT_BASE_URL,
+  production: process.env.BASE_URL ?? DEFAULT_BASE_URL
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((env) => ({
+  base: BASE_URL_MAP[env.mode],
   plugins: [react(), svgr()],
   server: {
     port: 5174
   }
-})
+}))
