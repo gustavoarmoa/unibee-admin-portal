2eb01dca2075417a6447845060fcd49e0981529d
The Mist <mist.zzh@gmail.com>
2024-10-13 23:59:11 +0800

    feat: change base url at runtime (#27)

--- start_with_env.sh
+++ start_with_env.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+sed -i -E 's@(window.__dynamic_base__=)[^<]+@\1\"'"$BASE_PATH"'\"@g' /usr/share/nginx/html/index.html
+
 find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,UNIBEE_API_URL,'"$UNIBEE_API_URL"',g' {} \;
 nginx -g "daemon off;"
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { dynamicBase } from 'vite-plugin-dynamic-base'
 import svgr from 'vite-plugin-svgr'
-
-const DEFAULT_BASE_URL = '/'
-
-const BASE_URL_MAP = {
-  development: DEFAULT_BASE_URL,
-  production: process.env.BASE_URL ?? DEFAULT_BASE_URL
-}
 
 // https://vitejs.dev/config/
 export default defineConfig((env) => ({
-  base: BASE_URL_MAP[env.mode],
-  plugins: [react(), svgr()],
+  base: env.mode === 'production' ? '/__dynamic_base__/' : '/',
+  plugins: [
+    react(),
+    svgr(),
+    dynamicBase({
+      // dynamic public path var string, default window.__dynamic_base__
+      publicPath: 'window.__dynamic_base__',
+      // dynamic load resources on index.html, default false. maybe change default true
+      transformIndexHtml: true
+      // provide conversion configuration parameters. by 1.1.0
+      // transformIndexHtmlConfig: { insertBodyAfter: false }
+    })
+  ],
   server: {
     port: 5174
   }
