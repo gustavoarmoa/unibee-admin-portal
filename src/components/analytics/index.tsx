b31146ef8527a9a10550d27e22e17e8ace354e8f
Edward <edward.wo@wowow.io>
2024-10-31 19:05:26 +0800

    feat: add analytics page

--- src/components/analytics/index.tsx
+++ src/components/analytics/index.tsx
@@ -1,4 +1,5 @@
 import { LoadingOutlined } from '@ant-design/icons'
+<<<<<<< HEAD
 import { Button, message, Modal, Spin } from 'antd'
 import dayjs from 'dayjs'
 import { useEffect, useState } from 'react'
@@ -43,10 +44,47 @@ const Index = () => {
     setLoading(true)
     const [rev, err] = await getRevenueReq()
     setLoading(false)
+=======
+import {
+  Button,
+  Col,
+  Form,
+  FormInstance,
+  Input,
+  Pagination,
+  Row,
+  Table,
+  message
+} from 'antd'
+import type { ColumnsType, TableProps } from 'antd/es/table'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getActivityLogsReq, getAnalyticsReportReq } from '../../requests'
+import { TActivityLogs } from '../../shared.types'
+
+import { formatDate } from '../../helpers'
+import { usePagination } from '../../hooks'
+import '../../shared.css'
+
+const PAGE_SIZE = 10
+const APP_PATH = import.meta.env.BASE_URL
+
+const Index = () => {
+  const navigate = useNavigate()
+  const [form] = Form.useForm()
+  const [total, setTotal] = useState(0)
+  const { page, onPageChange } = usePagination()
+  const [loading, setLoading] = useState(false)
+  const [logs, setLogs] = useState<TActivityLogs[]>([])
+
+  const getRevenue = async () => {
+    const [revenueRes, err] = await getAnalyticsReportReq()
+>>>>>>> 4792144 (feat: add analytics page)
     if (err != null) {
       message.error((err as Error).message)
       return
     }
+<<<<<<< HEAD
     setRevenue(rev)
   }
 
@@ -111,6 +149,17 @@ const Index = () => {
       </div>
     </div>
   )
+=======
+    console.log('res res: ', revenueRes)
+  }
+
+  useEffect(() => {
+    // fetchLogs()
+    getRevenue()
+  }, [])
+
+  return <div>analytics page</div>
+>>>>>>> 4792144 (feat: add analytics page)
 }
 
 export default Index
